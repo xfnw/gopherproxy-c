34cc9562a6c69fe6d18ec5f0ebc4b8dfb52327f2
xfnw <xfnw@riseup.net>
2022-07-08 11:13:24 -0400

    dark theme for input too

--- gopherproxy.c
+++ gopherproxy.c
@@ -703,7 +703,7 @@ main(void)
 		"Gopher HTTP proxy</title>\n"
 		"<style type=\"text/css\">\n"
 		"a { text-decoration: none; } a:hover { text-decoration: underline; }\n"
-		"@media (prefers-color-scheme: dark) { body { background-color: #000; color: #bdbdbd; } a { color: #56c8ff; } }\n"
+		"@media (prefers-color-scheme: dark) { body { background-color: #000; color: #bdbdbd; } a { color: #56c8ff; } input { border: 1px solid #777; color: #cdcdcd; background-color: #333 } }\n"
 		"</style>\n"
 		"<meta name=\"robots\" content=\"noindex, nofollow\" />\n"
 		"<meta name=\"robots\" content=\"none\" />\n"
